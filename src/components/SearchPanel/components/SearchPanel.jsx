44f526e35ffc8df59cf0c5a69ae70e072947f953
Mykhail Bobrykov <gelios238@gmail.com>
2021-05-06 20:47:23 +0300

    create SearchPanel

--- src/components/SearchPanel/components/SearchPanel.jsx
+++ src/components/SearchPanel/components/SearchPanel.jsx
@@ -1,19 +1,17 @@
 import {Link} from 'react-router-dom';
 import { Formik } from 'formik';
 
-import {wrapper, form, links_wrapper} from './SearchPanel.module.scss';
+import {wrapper, form, links_wrapper, button} from './SearchPanel.module.scss';
 import Likes from './Likes';
 import Dislikes from './Dislikes';
 import Favourites from './Favourites';
-
-
+import Search from './Search';
 
 const SearchPanel = () => {
 
 	return (
 		<div className={wrapper}>
 				<Formik
-					className={form}
 					initialValues={{ name: 'jared' }}
 					onSubmit={(values, actions) => {
 						console.log(1)
@@ -24,7 +22,7 @@ const SearchPanel = () => {
 					}}
 				>
 					{props => (
-						<form onSubmit={props.handleSubmit}>
+						<form onSubmit={props.handleSubmit} className={form}>
 							<input
 								type="text"
 								onChange={props.handleChange}
@@ -33,20 +31,16 @@ const SearchPanel = () => {
 								name="name"
 							/>
 							{/* {errors.name && touched.name && <div id="feedback">{errors.name}</div>} */}
-							<button type="submit" >Submit</button>
+							<button type="submit"  className={button}><Search/></button>
 						</form>
 					)}
 				</Formik>
 			<div className={links_wrapper}>
-				<Link to='/favourites'><Favourites/></Link>
-				<Link to='/dislikes'><Likes/></Link> 
-				<Link to='/likes'><Dislikes/></Link>
+				<Link to='/favourites' className={button}><Favourites/></Link>
+				<Link to='/dislikes' className={button}><Likes/></Link> 
+				<Link to='/likes' className={button}><Dislikes/></Link>
 			</div>
 		</div>
 	)
 }
 export default SearchPanel;
-
-{/* <Link to='/favourites'><img src={favourites} style={{fill: 'green'}} alt="favourites"/></Link> */}
-{/* <Link to='/likes'><img src={likes} alt="likes"/></Link>
-<Link to='/dislikes'><img src={dislikes} alt="dislikes"/></Link>  */}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Greetings} from './Greetings';
 export {default as Logo} from './Logo';
 export {default as NavItem} from './NavItem/index';
 export {default as SearchPanel} from './SearchPanel/container';
+export {default as BackButton} from './BackButton';
