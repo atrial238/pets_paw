eef4934b9a92608d024aaeb7ef828c786145c26d
Mykhail Bobrykov <gelios238@gmail.com>
2021-05-06 21:17:55 +0300

    create BackButton

---
+++ src/components/BackButton/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import Back from './Back';
+import {wrapper, title     } from './BackButton.moudle.scss';
+
+const BackButton = ({name}) => {
+	return (
+		<div className={wrapper}>
+			<Link to='/' ><Back/></Link>
+			<div className={title}>{name}</div>
+		</div>  
+	)
+}
+export default BackButton; 
+
+BackButton.propTypes = {
+	name: PropTypes.string,
+}
